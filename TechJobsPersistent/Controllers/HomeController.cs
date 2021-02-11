97bf21c26e4b31071498f97242dfe0ebd1f8da3f
KFrost <kristan.frost@gmail.com>
2021-02-11 12:23:53 -0600

    Final changes, I hope

--- TechJobsPersistent/Controllers/HomeController.cs
+++ TechJobsPersistent/Controllers/HomeController.cs
@@ -47,15 +47,25 @@ namespace TechJobsPersistent.Controllers
             
             This action needs to take in an instance of the AddJobViewModel and
             
-            IF the model is valid then
+            If the model is valid then
                 create a Job object that 
                     will pass the properties (Name, EmployerId, and Employer) from the ViewModel that it's taking in.
                    
                     - remember that the Employer is an employer object, so we have to search for that object in the Db using the EmployerId
 
+                
+                 When we add our Skills, we will need to iterate across all of the selected skills - a string array - (cause we can shoose more than one skill per job)
+                    Create a nw JobSkill with the properties of
+                        JobId being set to the ID of the new Job we're creating
+                        Job being set to the new Job we're creating,
+                        SkillID being set to the value of the skill we are currently looking at in the array (remember what type of array is it, that will probably need handled)
+                    Add this new skill to the JobSkills Db
+                    
+
                 Then we need to add this new Job object that we've crated to the Jobs table in the Db
-                   Then save the changes to the Db
-                   when all that works we'll redirect to see the list of Jobs
+                Then save the changes to the Db
+
+                when all that works we'll redirect to see the list of Jobs   
 
             If none of that happens (cause the model isn't valid), then we want to go to the Add view and 
             pass it the information that is in the View Model from the attempted addition
